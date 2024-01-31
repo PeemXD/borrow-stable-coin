92e427ab15fafd5be02df5f48beb153795c7b658
PeemXD <error074xd@gmail.com>
2024-01-31 20:50:33 +0700

    add file that auto created after verify code by sourcify

---
+++ verified-sources/0x1F6527c421b7fe37326B422DE9015B735c11Ccee/sources/contracts/PepoStablecoin.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+contract PepoStablecoin is ERC20, ERC20Permit, Ownable{
+
+    constructor() ERC20("Pepo Stablecoin", "USDP") ERC20Permit("USDP") Ownable(_msgSender()) {
+    }
+
+
+    function mint(address account, uint256 value) public onlyOwner {
+        _mint(account, value);
+    }
+
+    function burn(address account, uint256 value) public onlyOwner {
+        _burn(account, value);
+    }
+    
+}
