bcf633be68976ec7c1eeb2e5ae19d9a7e040eef8
PeemXD <error074xd@gmail.com>
2024-02-01 14:40:32 +0700

    add struct deptCollateralRatio and mapping deptCollateralRatios

--- contracts/BorrowStablecoin.sol
+++ contracts/BorrowStablecoin.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.20;
 
 contract BorrowStablecoin {
+    struct deptCollateralRatio {
+        uint dept;
+        uint collateral;
+    }
+
+    mapping(address => deptCollateralRatio) deptCollateralRatios;
 
 }
